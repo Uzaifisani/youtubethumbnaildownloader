4186dab1fd9767d5dbbfd061e2858e29c096a07d
Uzaifisani <67412481+Uzaifisani@users.noreply.github.com>
2020-10-11 16:07:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ function fetchImage()
     {	
 	htmlCode = `<div>
 	<img src="https://img.youtube.com/vi/${vCode}/0.jpg" alt="Youtube thumbnail" class="img-thumbnail"></br>
-    <button  type="button" class="btn btn-info" onclick="location.href='https://img.youtube.com/vi/${vCode}/maxresdefault.jpg'">Full HD Image</button>
-    <button type="button" class="btn btn-info" onclick="location.href='https://img.youtube.com/vi/${vCode}/hqdefault.jpg'">HD Image</button>
-    <button type="button" class="btn btn-info" onclick="location.href='https://img.youtube.com/vi/${vCode}/mqdefault.jpg'">360p Image</button>
+    <button  type="button" class="btn btn-info" onclick="window.open(href='https://img.youtube.com/vi/${vCode}/maxresdefault.jpg')">Full HD Image</button>
+    <button type="button" class="btn btn-info" onclick="window.open('https://img.youtube.com/vi/${vCode}/hqdefault.jpg')">HD Image</button>
+    <button type="button" class="btn btn-info" onclick="window.open(href='https://img.youtube.com/vi/${vCode}/mqdefault.jpg')">360p Image</button>
     </br></br>
     <button onclick="location.reload();" type="button" class="btn btn-success"">Fetch Again</button>
     </div>`
